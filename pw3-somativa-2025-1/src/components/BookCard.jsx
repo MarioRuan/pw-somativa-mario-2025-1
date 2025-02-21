20b11fabc6180bfdbf60561ee3055bcccdfb8be4
MarioRuan <marioruangoncalvesdasilva@gmail.com>
2025-02-20 21:43:46 -0300

    Adicionando estilos e o botão

--- pw3-somativa-2025-1/src/components/BookCard.jsx
+++ pw3-somativa-2025-1/src/components/BookCard.jsx
@@ -1,9 +1,13 @@
+import style from './BookCard.module.css'
+import Button from './Button'
+
 const BookCard = ({titulo,autor,imagem}) => {
     return(
-        <div>
-            <h3>{titulo}</h3>
-            <p>{autor}</p>
+        <div className={style.bookCard}>
+            <h3 className={style.titulo}>{titulo}</h3>
+            <p className={style.autor}>{autor}</p>
             <img src={imagem} alt="Capa: As Carvenas de Aço"/>
+            <Button label='DETALHE'/>
         </div>
     )
 }
