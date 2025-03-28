000bb52aa5a0b5fcef7fdaaab45210576fa91b55
MarioRuan <marioruangoncalvesdasilva@gmail.com>
2025-03-27 22:28:47 -0300

    Adicionando a API e o banco de dados

--- pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
+++ pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
@@ -3,10 +3,12 @@ import style from './CreateBook.module.css';
 import Input from "../form/Input";
 import Select from "../form/Select";
 import Button from "../form/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CreateBook = () =>{
     const [book,setBook] = useState({});
+
+    const [categories,setCategories] = useState([]);
 
     //Captura de dados do elemento de input
     function handlerChangeBook(event){
@@ -16,7 +18,7 @@ const CreateBook = () =>{
 
     //Captura de daods do elemento de select
     function handlerChangeCategory(event){
-        setBook({... book, category:event.target.options[event.target.selectedIndex].text})
+        setBook({... book, cod_categoria:event.target.options[event.target.selectedIndex].text})
         console.log(book)
     }
 
@@ -26,6 +28,22 @@ const CreateBook = () =>{
         console.log(book);
     }
 
+    //recupera os dados de categoria da apirest:
+    useEffect(()=>{
+        fetch('http://127.0.0.1:5000/listagemCateorias',{
+            method:"GET",
+            headers:{
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*'
+            }
+        }).then((response)=>{
+            console.log(response.json());
+        }).catch((error)=>{
+            console.log(error)
+        })
+    },[]);
+
     return(
         <section className={style.create_book_container}>
             <h1>CADASTRO DE LIVRO</h1>
@@ -33,31 +51,31 @@ const CreateBook = () =>{
                 <Input
                     text='Nome do livro'
                     type='text'
-                    name='txt_livro'
-                    id='txt_livro'
+                    name='nome_livro'
+                    id='nome_livro'
                     placeholder='Digite o nome do livro'
                     handlerChange={handlerChangeBook}
                 />
                 <Input
                     text='Autor do livro'
                     type='text'
-                    name='txt_autor'
-                    id='txt_autor'
+                    name='autor_livro'
+                    id='autor_livro'
                     placeholder='Digite o nome do autor do livro'
                     handlerChange={handlerChangeBook}
                 /> 
                 <Input
                     text='Descrição do livro'
                     type='text'
-                    name='txt_descricao'
-                    id='txt_descricao'
+                    name='descricao_livro'
+                    id='descricao_livro'
                     placeholder='Digite a descrição do livro'
                     handlerChange={handlerChangeBook}
                 />
 
                 <Select 
-                    name='slc_categotia'
-                    id='slc_categoria'
+                    name='cod_categoria'
+                    id='cod_categoria'
                     text='Categoria do livro'
                     handlerChange={handlerChangeCategory}
                 />
